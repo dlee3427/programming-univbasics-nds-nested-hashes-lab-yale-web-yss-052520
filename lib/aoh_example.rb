3ef84c399383f03845cad0507b291d2ac46cf70d
David Lee <david.lee.sl2634@yale.edu>
2020-05-15 23:45:16 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -8,11 +8,11 @@ BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 require 'pry'
 def assembled_aoh
   result = [
-    { name:  "Don Gately", occupation: "Live-in Staff"},
-    { name:  "Joelle van Dyne", occupation: "Radio Personality" },
-    { name:  "Pat Monteseian", occupation: "Staff" },
-    { name:  "Kate Gompert", occupation: "None" },
-    { name:  "Bruce Green", occupation: "Fan of Mildred" }
+    { :name =>"Don Gately", :occupation => "Live-in Staff"},
+    { :name => "Joelle van Dyne", :occupation => "Radio Personality" },
+    { :name => "Pat Monteseian", :occupation => "Staff" },
+    { :name => "Kate Gompert", :occupation => "None" },
+    { :name =>  "Bruce Green", :occupation => "Fan of Mildred" }
     ]
     return result 
   end
